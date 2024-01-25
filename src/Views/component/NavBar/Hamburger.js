be46f2f15d099e9e2a9bb850bf6fa91d16556e5d
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-01-26 00:26:00 +0100

    fin hamburgerButton

--- src/Views/component/NavBar/Hamburger.js
+++ src/Views/component/NavBar/Hamburger.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { Link } from 'react-router-dom';
 import '../../../css/component.css';
+import '../../../css/struct.css';
+
 
 export function Hamburger() {
     const [open, setOpen] = useState(false);
     const change = () => {
         setOpen(!open);
     }
-    return <button type="button" className="hamburger" onClick={change}></button>
+    return <div className="HamburgerStack">
+        <button type="button" className="hamburger" onClick={change}></button>
+        <div className="HamburgerStyle">
+            <div className="HamburgerBarre"></div>
+            <div className="HamburgerBarre"></div>
+            <div className="HamburgerBarre"></div>
+        </div>
+    </div>
 }
