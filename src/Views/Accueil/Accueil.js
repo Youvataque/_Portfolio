aa2600e4c779ab7e059bdf26b66ec0f54de50d50
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-01-25 03:39:36 +0100

    Ajout du responsive sauf pour la navBar

--- src/Views/Accueil/Accueil.js
+++ src/Views/Accueil/Accueil.js
@@ -9,24 +9,25 @@ import TempContext from '../../TempContext';
 export function Accueil() {
     const { temp, setTemp } = useContext(TempContext);
     return <div>
-       <div className='homePresentationRow'>
-            <div className='homePresentationStack'>
-                <img className='App-logo' src={temp == "dark"? darkBack : lightBack}/>
-                <div className='homePresentation'> 
-                    <p className='homeHello'>Hey there! I'm -</p>
-                    <p className='homeName'>Yannis</p>
-                    <p className='homeName'>SEGUIN</p>
-                    
+       <div className='GlobalHome'>
+            <div className='homePresentationRow'>
+                <div className='homePresentationStack'>
+                    <img className='App-logo' src={temp == "dark"? darkBack : lightBack}/>
+                    <div className='homePresentation'> 
+                        <p className='homeHello'>Hey there! I'm -</p>
+                        <p className='homeName2'>Yannis</p>
+                        <p className='homeName'>SEGUIN</p>
+                    </div>
                 </div>
+                <img className='profilPicBig' src={profilPicBig}/>
             </div>
-            <img className='profilPicBig' src={profilPicBig}/>
+            <div className='TextLength'>
+                <p className='homeTextPr'>
+                    <b> Junior mobile developper.</b>
+                    &nbsp;Passionate about programming, once I start working, nothing can stop me! Learning is, for me, the greatest pleasure.
+                </p>
+            </div>
        </div>
-       <div className='TextLength'>
-            <p className='homeTextPr'>
-                <b> Junior mobile developper.</b>
-                &nbsp;Passionate about programming, once I start working, nothing can stop me! Learning is, for me, the greatest pleasure.
-            </p>
-        </div>
         <p className='monText'>test</p>
         <p className='monText'>test</p>
         <p className='monText'>test</p>
