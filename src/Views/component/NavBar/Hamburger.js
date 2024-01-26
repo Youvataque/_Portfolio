8a9449d757fa56fd1422aa4a5b4e26e61c9e90cc
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-01-26 14:48:19 +0100

    100% css navBar

--- src/Views/component/NavBar/Hamburger.js
+++ src/Views/component/NavBar/Hamburger.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from 'react-router-dom';
 import '../../../css/component.css';
 import '../../../css/struct.css';
+import '../../../css/text.css';
 
 
 export function Hamburger() {
@@ -10,33 +11,31 @@ export function Hamburger() {
         setOpen(!open);
     }
     return <div>
-         <div className={`BlurHamburgerContent ${open ? "blur-active" : ""}`}>
-        </div>
-        
-            <div className={`menu ${open ? "open" : ""}`}>
-                {/* Les liens ou le contenu du menu ici */}
-                <ul className="">
-                    <li className="nav-item">
-                    <Link className='nav-link' to='/'>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className='nav-link' to='/projects'>Projects</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className='nav-link' to='/about'>About</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className='nav-link' to='/contact'>Contact</Link>
-                    </li>
-                </ul>
-                <div className={`HamburgerStack ${open ? "open" : ""}`}>
-            <button type="button" className="hamburger" onClick={change}></button>
-            <div className="HamburgerStyle">
-                <div className={`HamburgerBarreL ${open ? "open" : ""}`}></div>
-                <div className={`HamburgerBarreR ${open ? "open" : ""}`}></div>
-                <div className={`HamburgerBarre ${open ? "open" : ""}`}></div>
+        <div className={`BlurHamburgerContent ${open ? "blur-active" : ""}`}></div>
+        <div className={`menu ${open ? "open" : ""}`}>
+            <h1 className="HamburgerH1">Welcome,<br/>User !</h1>
+            <ul className="HamburgerContentDirector">
+                <li className="HamburgerNavItem">
+                    <Link className='HamburgerNavLink' to='/' onClick={change}><p className="HamburgerPoint">1.&nbsp;</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home</Link>
+                </li>
+                <li className="HamburgerNavItem">
+                    <Link className='HamburgerNavLink' to='/projects' onClick={change}><p className="HamburgerPoint">2.&nbsp;</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Projects</Link>
+                </li>
+                <li className="HamburgerNavItem">
+                    <Link className='HamburgerNavLink' to='/about' onClick={change}><p className="HamburgerPoint">3.&nbsp;</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;About</Link>
+                </li>
+                <li className="HamburgerNavItem">
+                    <Link className='HamburgerNavLink' to='/contact' onClick={change}><p className="HamburgerPoint">4.&nbsp;</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact</Link>
+                </li>
+            </ul>
+            <div className={`HamburgerStack ${open ? "open" : ""}`}>
+                <button type="button" className="hamburger" onClick={change}></button>
+                <div className="HamburgerStyle">
+                    <div className={`HamburgerBarreL ${open ? "open" : ""}`}></div>
+                    <div className={`HamburgerBarreR ${open ? "open" : ""}`}></div>
+                    <div className={`HamburgerBarre ${open ? "open" : ""}`}></div>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 }
