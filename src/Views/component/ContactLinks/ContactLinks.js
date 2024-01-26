cbd7158c00662ef78d8fd6ec7ad26038baa218b3
SEGUIN Yannis <102596878+Youvataque@users.noreply.github.com>
2024-01-26 23:23:16 +0100

    petit fix :)

--- src/Views/component/ContactLinks/ContactLinks.js
+++ src/Views/component/ContactLinks/ContactLinks.js
@@ -5,7 +5,7 @@ export function ContactLinks({titles, links}) {
     return <div className='ContactsLinksBodyDirector'>
         <ul className='ContactLinksBody'>
             {titles.map((title, index) => (
-                <motion.li animate={{y : [100, 0], opacity : 1}} transition={{delay : (index + 1) / 6}} className="ContactLinks" key={index} href={links[index]} target='_blank' >{title}</motion.li>
+                <motion.li animate={{y : [200, 0], opacity : 1}} transition={{y : {delay : (index + 1) / 6}, opacity : {delay : (index + 1) / 6}}} className="ContactLinks" key={index}><a href={links[index]} target='_blank'>{title}</a></motion.li>
             ))}
         </ul>
     </div>
